4eab8cbbb290441e2022d7755524131f6ebb1a2d
Jhonatan Villamizar <jhonatan.avj@gmail.com>
2024-06-01 21:41:25 -0500

    Proyecto: proceso de Sign-Up con GraphQL aunque los archivos de postman no los dejaron en recursos

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,25 @@
 "use server"
 
-export const handelCreateUser = (formData: any) => {
-    console.log("handelCreateUser", formData);
+import { GraphQLClientSingleton } from "app/graphql"
+import { createUserMutation } from "app/graphql/mutations/createUserMutations"
+
+export const handelCreateUser = async (formData: FormData) => {
+    const formDataObject = Object.fromEntries(formData)
+    delete formDataObject["password_confirmation"]
+    
+    const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+
+    const variables = {
+        input: {
+            ...formDataObject,
+            fistName: "John",
+            lastName: "smith",
+            email: "johnsmith@shopify.com",
+            phone: "+57" + formDataObject.phone,
+            password: "shopify",
+        }
+    }
+
+    const data = await graphqlClient.request(createUserMutation, variables)
+    console.log(data)
 }
