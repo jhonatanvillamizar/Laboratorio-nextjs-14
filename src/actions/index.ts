842cfc7c2dc72ce0eb328c5f629fba41193d4f5f
Jhonatan Villamizar <jhonatan.avj@gmail.com>
2024-06-05 20:22:34 -0500

    Manejo de cookies para colocar un token de acceso de un proyecto en Next.js aunque no estan los archivos postman y no funciona la parte de signup

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,6 +2,8 @@
 
 import { GraphQLClientSingleton } from "app/graphql"
 import { createUserMutation } from "app/graphql/mutations/createUserMutations"
+import { createAccessToken } from "app/utils/auth/createAccessToken"
+import { redirect } from "next/navigation"
 
 export const handelCreateUser = async (formData: FormData) => {
     const formDataObject = Object.fromEntries(formData)
@@ -12,14 +14,15 @@ export const handelCreateUser = async (formData: FormData) => {
     const variables = {
         input: {
             ...formDataObject,
-            fistName: "John",
-            lastName: "smith",
-            email: "johnsmith@shopify.com",
             phone: "+57" + formDataObject.phone,
-            password: "shopify",
         }
     }
 
-    const data = await graphqlClient.request(createUserMutation, variables)
-    console.log(data)
+    const { customerCreate } = await graphqlClient.request(createUserMutation, variables)
+    const { customerUserErrors, customer } = customerCreate
+    
+    if(customer?.fistName){
+        await createAccessToken(formDataObject.email as string, formDataObject.password as string)
+        redirect('/store')
+    }
 }
